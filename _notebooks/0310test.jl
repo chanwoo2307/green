1d6f4e2d55615b84a5fad9faa9cd2db32b84df4e
chanwoo3309 <92509506+202021340@users.noreply.github.com>
2022-03-11 23:51:25 +0900

    Create 0310test.jl

---
+++ _notebooks/0310test.jl
@@ -0,0 +1,32 @@
+### A Pluto.jl notebook ###
+# v0.18.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0427af5a-a14a-11ec-3cd4-a92623da9071
+#3월 10일 통계전산#
+
+# ╔═╡ 03d197c0-534a-4a84-ae44-570885cadee7
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═0427af5a-a14a-11ec-3cd4-a92623da9071
+# ╠═03d197c0-534a-4a84-ae44-570885cadee7
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
