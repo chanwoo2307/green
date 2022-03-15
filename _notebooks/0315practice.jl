f125216be7a4f3baa0209237562b4f5c0eb16519
chanwoo2307 <chanwoo2307@gmail.com>
2022-03-15 22:56:41 +0900

    .

---
+++ _notebooks/0315practice.jl
@@ -0,0 +1,28 @@
+### A Pluto.jl notebook ###
+# v0.18.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4fc833b0-a467-11ec-04c5-61fe210806e8
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═4fc833b0-a467-11ec-04c5-61fe210806e8
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
