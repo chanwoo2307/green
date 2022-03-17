aab3062cd7dc2b556485c8d2fad7e510c1bac7da
chanwoo2307 <chanwoo2307@gmail.com>
2022-03-17 23:58:53 +0900

    Create 0317practice.jl

---
+++ _notebooks/0317practice.jl
@@ -0,0 +1,51 @@
+### A Pluto.jl notebook ###
+# v0.18.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ bf6b2464-0345-4492-b3e4-b9c8b28f5a23
+z=1
+
+# ╔═╡ 55019efc-9118-4407-a57b-54e26ad4a710
+function f(z)
+	return z+100 
+	# 위에서 z를 선언하였지만 다시 z를 재 정의할 수 있다?
+end
+
+# ╔═╡ 648bd507-c2ff-40b5-89f9-000f9bb7e720
+f(1)
+
+# ╔═╡ 2391f74f-26f7-465e-a317-9632c760d2fa
+z
+
+# ╔═╡ ce7fe23d-b4cc-4de9-acc6-ab7c394af4d8
+
+
+# ╔═╡ 8392b8de-d722-4137-91f1-6ee1a6926437
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═bf6b2464-0345-4492-b3e4-b9c8b28f5a23
+# ╠═55019efc-9118-4407-a57b-54e26ad4a710
+# ╠═648bd507-c2ff-40b5-89f9-000f9bb7e720
+# ╠═2391f74f-26f7-465e-a317-9632c760d2fa
+# ╠═ce7fe23d-b4cc-4de9-acc6-ab7c394af4d8
+# ╠═8392b8de-d722-4137-91f1-6ee1a6926437
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
